68a50143361a751a7ca9b920f79327beab653a75
Jabulani-N <jabu.94@gmail.com>
2023-04-12 16:40:13 -0700

    outer_shell_created.-runs_until_user_says_exit

---
+++ includeme.h
@@ -0,0 +1,18 @@
+/*I'm the header file for this entier folder*/
+
+#ifndef anythingHere_H
+#define anythingHere_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int disunifiedField (char **environment);
+
+#endif
+
