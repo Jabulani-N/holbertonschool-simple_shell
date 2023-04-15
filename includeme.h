f35946a9e51fd02b908e1febeb228a0ffe511e35
adel <6126.holbertonstudents.com>
2023-04-15 13:31:45 -0700

    man and Authors

--- includeme.h
+++ includeme.h
@@ -1,17 +1,34 @@
 /*I'm the header file for this entier folder*/
+<<<<<<< HEAD
 #ifndef INCLUDEME_H
 #define INCLUDEME_H
 #endif
 #ifndef anythingHere_H
 #define anythingHere_H
+=======
+
+#ifndef anythingHere_H
+#define anythingHere_H
+
+>>>>>>> 68a50143361a751a7ca9b920f79327beab653a75
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <sys/types.h>
 #include <unistd.h>
+<<<<<<< HEAD
 
 #define BUFFER 1024
 
 
 #endif
+=======
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int disunifiedField (char **environment);
+
+#endif
+
+>>>>>>> 68a50143361a751a7ca9b920f79327beab653a75
