cb0a2fea645357ceb3a61dd3a511b107b04ae7ef
Jabulani-N <jabu.94@gmail.com>
2023-04-18 07:59:14 -0700

    loops,_but_doesnt_exit

--- open_argument.c
+++ open_argument.c
@@ -5,20 +5,36 @@
  * @inputArray: idk
  * Return: Always 0.
  */
+
+/*
+ *		known bugs
+ * it forks even if it is not able to run, like a directory
+ * 	Try making a child's return value from this function cause the caller to just straight exit everything
+ * 		can do this via continue
+ */
 int openWar(char **inputArray)
 {
+	int adultsOnly;
 /*	inputArray++;*/
-	if (!access(inputArray[0], X_OK))
+	if (!access(inputArray[0], X_OK),)
 	{
 
 /*		printf("yes access.\nBefore execve\n");*/
 /*		forking_for_filicide(inputArray);*/
 		/* the old code in question is below, commented out*/
-		if (execve(inputArray[0], inputArray, NULL) == -1)
+		adultsOnly = fork();
+		if (adultsOnly == 0)
 		{
-			perror("Error");
+			if (execve(inputArray[0], inputArray, NULL) == -1)
+			{
+				perror("Error");
+			}
+/*			printf("After execve\n");*/
 		}
-/*		printf("After execve\n");*/
+		else
+		{
+			wait(&adultsOnly);
+		}
 	}
 	else
 		perror("Error");/* printf("no access\n");*/
